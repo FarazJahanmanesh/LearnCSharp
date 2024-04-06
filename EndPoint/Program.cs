81ce72435af25d1f4da2a048376b94d16c4d9299
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2024-04-06 12:02:18 +0330

    with

--- EndPoint/Program.cs
+++ EndPoint/Program.cs
@@ -8,6 +8,9 @@ a.Name = "faraz";
 a.Email = "farazebipax@gmail.com";
 a.Id = 1;
 
+User b = a with { Password = "F@razmj82" };
+//here we can have our user but change the password with 'with'
+
 Console.WriteLine(a);
 Console.WriteLine(a.GetType());
 
