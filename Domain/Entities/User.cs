6066b9a369483dc5c7744298911e03f4a9ef3c69
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2024-04-06 11:54:23 +0330

    Add project files.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public record User
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ EndPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+
+User a = new User();
+a.Name = "faraz";
+a.Email = "farazebipax@gmail.com";
+a.Password = "farazmj82";
+a.Id = 1;
+
+
+
+Console.WriteLine(a);
+Console.WriteLine(a.GetType());
+
+Console.WriteLine(a.Id);
+Console.WriteLine(a.Name);
+Console.WriteLine(a.Email);
+Console.WriteLine(a.Password);
